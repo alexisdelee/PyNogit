b453cc566794b99db134181929ff775a6b35b1b9
Alexis Delee <alexisdelee@protonmail.com>
2019-02-05 12:40:27 +0100

    update credentials for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,4 @@ from pynogit import NoGit
 
 @pytest.fixture
 def db():
-    return NoGit(username = "master", credentials = "master", database = "mcdo")
+    return NoGit(username = "4NERU7V7ua", credentials = "KumW6nPPRA", database = "p2ouvFBbx6")
